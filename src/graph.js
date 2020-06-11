52b535b28c6ba20db1c3edff14df811c977e50f3
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-06-11 11:39:32 +0530

    fixed failing tests added function insertChildrenIntoQueue and using

--- src/graph.js
+++ src/graph.js
@@ -3,26 +3,31 @@
 // Source => from
 // To => to
 // Should return true.
-const makeGraph = (graph, [node, connectedTo]) => {
+const makeGraph = (graph, [node, child]) => {
   graph[node] = graph[node] || [];
-  graph[node].push(connectedTo);
+  graph[node].push(child);
   return graph;
 };
+
+const insertChildrenIntoQueue = (queue, visited, children) => {
+  if (!children) return;
+  for (const child of children) {
+    !queue.includes(child) && !visited.includes(child) && queue.push(child);
+  }
+};
+
 const bfs = (pairs, source, target) => {
   const graph = pairs.reduce(makeGraph, {});
   const visited = [];
-  const queue = [source];
+  const queue = [];
   let result = false;
+  insertChildrenIntoQueue(queue, visited, graph[source]);
   while (queue.length && !result) {
     const node = queue.shift();
-    if (node == target) result = true;
     if (visited.includes(node)) continue;
+    if (node == target) result = true;
     visited.push(node);
-    const children = graph[node];
-    children.forEach(
-      child =>
-        !queue.includes(child) && !visited.includes(child) && queue.push(child)
-    );
+    insertChildrenIntoQueue(queue, visited, graph[node]);
   }
   return result;
 };
