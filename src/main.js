3048ebb0694cc5a52e05dcaf093e18b465620810
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-06-22 23:07:34 +0530

    Implemented Dijkstras shortest path algorithm

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const { bfs, dfs, findPath } = require('./graph');
-const { primsMST, kruskalMST } = require('./weighted_graphs');
+const {
+  primsMST,
+  kruskalMST,
+  dijkstrasShortestPath,
+} = require('./weighted_graphs');
 
 const parseInputMakePairs = data => {
   const pairs = data.map(str => str.replace(/\|/g, '').trim().split('  '));
@@ -10,7 +14,6 @@ const parseInputMakePairs = data => {
 const main = () => {
   const data = fs.readFileSync('./src/data.txt', 'utf-8').trim().split('\n');
   const pairs = parseInputMakePairs(data);
-  console.log(pairs);
   const questions = [
     ['A', 'M'],
     ['A', 'J'],
@@ -27,12 +30,20 @@ const main = () => {
     path.forEach((p, i) => process.stdout.write(`${p} -> `));
     console.log('\n');
   }
+
   let mst = primsMST(pairs, false);
   console.log('Prims MinimumSpanningTree\n', mst.newAdjacencyList);
   console.log('Prims Total Weight', mst.totalWeight);
+
   mst = kruskalMST(pairs, false);
-  console.log('Kruskal MinimumSpanningTree\n', mst.newAdjacencyList);
+  console.log('\nKruskal MinimumSpanningTree\n', mst.newAdjacencyList);
   console.log('Kruskal Total Weight', mst.totalWeight);
+  
+  const { table, path, weight } = dijkstrasShortestPath(pairs, true, 'A', 'J');
+  console.table("\nDijkstra's Minimum Path to all nodes from A");
+  console.table(table);
+  console.log("Dijkstra's Shortest Path from A to J", path);
+  console.log("Dijkstra's Shortest weight from A to J", weight);
 };
 
 main();
--- src/weighted_graphs.js
+++ src/weighted_graphs.js
@@ -34,9 +34,18 @@ const getMinimumEdge = (adjacencyList, visited) => {
   return minEdge;
 };
 
+const extractVertices = pairs => {
+  const vertices = new Set();
+  for (const pair of pairs) {
+    vertices.add(pair[0]);
+    vertices.add(pair[1]);
+  }
+  return Array.from(vertices);
+};
+
 const primsMST = (pairs, isDirected) => {
   const adjacencyList = getAdjacencyList(pairs, isDirected);
-  const allNodes = Object.keys(adjacencyList);
+  const allNodes = extractVertices(pairs);
   const visited = [];
   const mstEdges = [];
   let currentEdge = allNodes[0];
@@ -71,4 +80,71 @@ const kruskalMST = (pairs, isDirected) => {
   return { newAdjacencyList, totalWeight };
 };
 
-module.exports = { primsMST, kruskalMST };
+const createTable = (allEdges, source) => {
+  const table = {};
+  for (const edge of allEdges) {
+    table[edge] = { source: edge, weight: Infinity, parent: undefined };
+  }
+  table[source].weight = 0;
+  return table;
+};
+
+const updateTable = (table, node, weight, parent) => {
+  table[node] = table[node] || {};
+  const existingWeight = table[node].weight;
+  const existingParent = table[node].parent;
+  table[node].weight = existingWeight <= weight ? existingWeight : weight;
+  table[node].parent = existingWeight <= weight ? existingParent : parent;
+};
+
+const getMinimumOfTable = (table, processedNodes) => {
+  let nodes = Object.values(table);
+  nodes = nodes.filter(edge => !processedNodes.includes(edge.source));
+  return nodes.reduce((min, node) => (node.weight < min.weight ? node : min), {
+    weight: Infinity,
+  });
+};
+
+const getPath = (table, node, source, path) => {
+  if (node === source) return path;
+  const parent = table[node].parent;
+  path.unshift(parent);
+  return getPath(table, parent, source, path);
+};
+
+const modifyTableWithPaths = (table, source) => {
+  let nodes = Object.values(table);
+  const newTable = {};
+  for (const node of nodes) {
+    let path = getPath(table, node.source, source, []);
+    path = path.reduce((p, n) => p + n + '->', '');
+    path += node.source;
+    newTable[node.source] = { weight: node.weight, path };
+  }
+  return newTable;
+};
+
+const dijkstrasShortestPath = (pairs, isDirected, source, target) => {
+  const adjacencyList = getAdjacencyList(pairs, isDirected);
+  let allEdges = extractVertices(pairs);
+  let table = createTable(allEdges, source);
+  const processedNodes = [];
+  let nextNode = source;
+  while (processedNodes.length != allEdges.length - 1) {
+    let children = adjacencyList[nextNode] || [];
+    children = children.filter(child => !processedNodes.includes(child.edge));
+    const weight = table[nextNode].weight;
+    for (const child of children) {
+      const weightToChild = weight + child.weight;
+      updateTable(table, child.edge, weightToChild, nextNode);
+    }
+    processedNodes.push(nextNode);
+    const minimumEdge = getMinimumOfTable(table, processedNodes);
+    if (minimumEdge.weight === Infinity) continue;
+    nextNode = minimumEdge.source;
+  }
+  table = modifyTableWithPaths(table, source);
+  return { table, path: table[target].path, weight: table[target].weight };
+};
+
+module.exports = { primsMST, kruskalMST, dijkstrasShortestPath };
